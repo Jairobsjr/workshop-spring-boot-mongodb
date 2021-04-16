6e5ca4b63d9365db9fba42e3c2dcb30211a8e512
Jairo <jairo.jb@gmail.com>
2021-04-15 21:30:17 -0300

    projeto criado

---
+++ src/test/java/com/jairo/workshopmong/WorshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jairo.workshopmong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
