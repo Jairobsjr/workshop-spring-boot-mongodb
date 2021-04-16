0448dfd41fd57cd3b3c38db4ad7607856bf2a9e5
Jairo <jairo.jb@gmail.com>
2021-04-15 22:02:17 -0300

    Entity User e REST funcionando

--- src/main/java/com/jairo/workshopmongo/WorshopmongoApplication.java
+++ src/main/java/com/jairo/workshopmongo/WorshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.jairo.workshopmong;
+package com.jairo.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
