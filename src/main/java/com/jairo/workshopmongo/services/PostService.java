fe0e774198fb47dd13250a78d1edab627bb33a3e
Jairo <jairo.jb@gmail.com>
2021-04-17 11:41:02 -0300

    Obtendo um post por id

---
+++ src/main/java/com/jairo/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.jairo.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jairo.workshopmongo.domain.Post;
+import com.jairo.workshopmongo.repository.PostRepository;
+import com.jairo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	public PostRepository postRepository;
+	
+	public List<Post> findAll() {
+		return postRepository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> user = postRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
+}
